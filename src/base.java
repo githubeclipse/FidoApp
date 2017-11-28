c9f5538b50f0003d067c1ee4857699c99d6832a6
Vipin Malhotra <Vipin.Malhotra@rci.rogers.ca>
2017-11-27 23:16:58 -0500

    first commit

---
+++ src/base.java
@@ -0,0 +1,26 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class base {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android device");
+		cap.setCapability(MobileCapabilityType.APP, "C:\\Users\\Vipin.Malhotra\\Desktop\\FidoApp\\src\\My Account_com.fidosolutions.myaccount.apk");
+				
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+		driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+		
+		
+	}
+
+}
